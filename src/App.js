bfe69f6573cbb18c332d6d5fd54533cd38bf9905
Alexander Fedorov <fedlogic@gmail.com>
2021-07-08 14:41:52 +1200

    Adding category search and reset button

--- src/App.js
+++ src/App.js
@@ -1,34 +1,67 @@
 import './App.css';
-import useSWR from 'swr';
+import useSWR, { SWRConfig } from 'swr';
+import React from 'react';
 
 //custom fetcher function which returns a promise
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
+
 export default function App() {
-
-  const url = 'https://jsonplaceholder.typicode.com/posts/?_limit=50'
-  const { data, error } = useSWR(url, fetcher)
-  console.log('What is it', data, 'errorDisplay?', error)
-
-  if (error) {
-    return <div>Error...</div>
-  } else if (!data) {
-    return <div>Loading...</div>
-  }
-
-
-
   return (
-    <>
-
-      <pre>{JSON.stringify(data, null, 4)}</pre>
-
-    </>
+    <SWRConfig value={{ revalidateOnFocus: false, fetcher }}>
+      <Crimes />
+    </SWRConfig>
   );
 }
 
-function Response() {
+function Crimes() {
+  const url =
+    "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10";
+  const { data, error } = useSWR(url);
 
+  if (error) return <div>Error...</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <DisplayCrimes
+      crimes={data}
+      categories={[...new Set(data.map(crime => crime.category))]}
+    />
+  );
+}
+
+function DisplayCrimes({ crimes, categories }) {
+  const [filterCategory, setFilterCategory] = React.useState(null);
+
+  const filteredCrimes = filterCategory
+    ? crimes.filter(crime => crime.category === filterCategory)
+    : crimes;
+
+  return (
+    <>
+      {categories.map(category => (
+        <button
+          onClick={() => {
+            setFilterCategory(category);
+          }}
+          key={category}
+        >
+          {category}
+        </button>
+      ))}
+      {filterCategory && (
+        <button
+          onClick={() => {
+            setFilterCategory(null);
+          }}
+        >
+          reset
+        </button>
+      )}
+
+      <pre>{JSON.stringify(filteredCrimes, null, 2)}</pre>
+    </>
+  );
 }
 
 // const fetcher = (...args) => fetch(...args).then(res => res.json())
